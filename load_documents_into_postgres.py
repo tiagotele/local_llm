161478a6386c10561f81669789734f9213ba195c
Tiago <tiagotele@gmail.com>
2024-10-08 18:17:02 -0300

    Code refactor

--- load_documents_into_postgres.py
+++ load_documents_into_postgres.py
@@ -1,57 +1,75 @@
-import psycopg2
+import logging
 import os
 
-from llama_index.core import Settings
-from llama_index.core import VectorStoreIndex
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+import psycopg2
 from sentence_transformers import SentenceTransformer
 
-DB_NAME = 'postgres'
-DB_USER = 'postgres'
-DB_PASSWORD = 'postgres'
-DB_HOST = 'localhost'
+logging.basicConfig(
+    level=logging.DEBUG, 
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("app.log"),
+        logging.StreamHandler(),
+    ],
+)
+
+# Create a logger object
+logger = logging.getLogger(__name__)
+
+DB_NAME = "postgres"
+DB_USER = "postgres"
+DB_PASSWORD = "postgres"
+DB_HOST = "localhost"
 DB_PORT = 5432
+FOLDER_WITH_DATA = "data"
 
-# Set locall llama
-# embedding_model = HuggingFaceEmbedding(model_name="sentence-transformers/all-MiniLM-L6-v2")
-# embedding_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
-# Settings.embed_model = embedding_model
 
-def connect_to_db():
+
+def connect_to_db(
+    db_name: str,
+    db_user: str,
+    db_password: str,
+    db_host: str = "localhost",
+    db_port: int = 5432,
+):
     connection = psycopg2.connect(
-        dbname=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        host=DB_HOST,
-        port=DB_PORT
+        dbname=db_name, user=db_user, password=db_password, host=db_host, port=db_port
     )
-    return  connection
+    return connection
 
-def insert_item(connection, description, embedding):
-    
+
+def insert_item(connection, file_name, embedding):
+
     with connection.cursor() as cursor:
         embedding_str = str(embedding)
         cursor.execute(
             "INSERT INTO items_test (description, embedding) VALUES (%s, %s)",
-            (description, embedding_str)
+            (file_name, embedding_str),
         )
         connection.commit()
+        logger.info(f"File {file_name} inserted.")
+
+
+def load_files_from_folder(folder_name: str = "data"):
+
+    files_in_folder = [file for file in os.listdir(folder_name)]
+
+    for file in files_in_folder:
+
+        with open(f"{folder_name}/{file}", "r") as file_reader:
+            file_content = file_reader.readlines()
+            embedded = embedding_model.encode(file_content[0]).tolist()
+            insert_item(
+                connect_to_db(DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT),
+                file,
+                str(embedded),
+            )
+
 
 if __name__ == "__main__":
-    print("main")
-    files_in_folder = [f for f in os.listdir("data") ]
-    print(files_in_folder)
-    try: 
-        for f in files_in_folder:
-            print(f"file = {f}")
-            with open(f"data/{f}", "r") as file:
-                file_content = file.readlines()
-                embedded_str = str(embedding_model.encode(file_content[0]).tolist())
-                # embedded_str = embedded_str.replace(" ", "")
-                print("\n\n\n\n")
-                # print(embedded_str.tolist())
-                # print(f)
-                insert_item(connect_to_db(), f, embedded_str)
+    try:
+        load_files_from_folder(FOLDER_WITH_DATA)
     except Exception as e:
-        print(e)
+        logger.error(f"Error inserting file: {e}")
+        raise e
